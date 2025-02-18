585c4dc69772c1c28e3684c231bd05da796ff0df
Maciej <maciekjur123@gmail.com>
2025-02-18 22:26:32 +0100

    Added angle system of boat and wind, attack angle calculation

---
+++ Sailboat/Assets/Scripts/BoatController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+public class BoatController : MonoBehaviour
+{
+    public float turnSpeed = 50f;
+    private WindSystem _wind;
+    public float logInterval = 1f; 
+    private float nextLogTime = 0f; 
+    
+    private void Start()
+    {
+        _wind = FindObjectOfType<WindSystem>();
+    }
+
+    void Update()
+    { 
+        float turnInput = Input.GetAxis("Horizontal");
+        float rotationAmount = turnInput * turnSpeed * Time.deltaTime;
+
+        transform.Rotate(0, rotationAmount, 0);
+        if (Time.time >= nextLogTime)
+        {
+            Debug.Log( "Attack angle: " + CalculateAttackAngle());
+        }
+    }
+
+    // V angle
+    private float CalculateAttackAngle()
+    {
+        float boatAngle = transform.eulerAngles.y;
+        float windAngle = _wind.windAngle;
+
+        float diff = boatAngle - windAngle;
+        float attackAngle;
+        if (diff >= 0)
+        {
+            attackAngle = diff;
+        }
+        else
+        {
+            attackAngle = 360 + diff;
+        }
+
+        return attackAngle;
+    }
+}
---
+++ Sailboat/Assets/Scripts/WIndSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WindSystem : MonoBehaviour
+{
+    public float windAngle = 0f; 
+    public float windSpeedKnots = 10f; 
+    
+    public float GetWindSpeedMS()
+    {
+        return windSpeedKnots * 0.5144f; 
+    }
+}
