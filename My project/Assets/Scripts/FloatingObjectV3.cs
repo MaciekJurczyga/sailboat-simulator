2e28df9f479ffb96355417489b68d3ab51170da9
Maciej <maciekjur123@gmail.com>
2025-06-30 15:10:47 +0200

    Add dumping force to bouyancy script to fix too high bouyancy on high boat velcoity

--- My project/Assets/Scripts/FloatingObjectV3.cs
+++ My project/Assets/Scripts/FloatingObjectV3.cs
@@ -3,75 +3,73 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class FloatingObjectV3 : MonoBehaviour
 {
+    [Header("Pływaki (Floaters)")]
     public Transform[] floaters;
-    
+
+    [Header("Ustawienia Wyporności")]
+    public float floatingPower = 15f;
+    public float dampingPower = 5f; 
+
+    [Header("Ustawienia Oporu (Drag)")]
     public float underWaterDrag = 3f;
     public float underWaterAngularDrag = 1f;
     public float airDrag = 0f;
     public float airAngularDrag = 0.05f;
 
-    private Rigidbody m_Rigidbody;
-
-    public float floatingPower = 15f;
-    private bool underwater;
-    
+    [Header("Ustawienia Fal (Lux Water)")]
     public float timeOffset;
     public Material waterMaterial;
 
+    private Rigidbody m_Rigidbody;
     private int floatersUnderWater;
+    
+
+    private LuxWaterUtils.GersterWavesDescription waveDescription;
+
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        
+        waveDescription = new LuxWaterUtils.GersterWavesDescription();
+        LuxWaterUtils.GetGersterWavesDescription(ref waveDescription, waterMaterial);
     }
-    
+
     void FixedUpdate()
     {
         floatersUnderWater = 0;
+        
         for (int i = 0; i < floaters.Length; i++)
         {
-            float waveHeight = getWaveHeightAtPoint(floaters[i].position);
+
+            Vector3 waveDisplacement = LuxWaterUtils.GetGestnerDisplacement(floaters[i].position, waveDescription, timeOffset);
+            float waveHeight = waveDisplacement.y; 
+
             float diff = floaters[i].position.y - waveHeight;
+
             if (diff < 0)
             {
-                m_Rigidbody.AddForceAtPosition(floatingPower * Mathf.Abs(diff) * Vector3.up, floaters[i].position, ForceMode.Force);
-                floatersUnderWater += 1;
-                if (!underwater)
-                {
-                    underwater = true;
-                    SwitchState(true);
-                }
+               
+                m_Rigidbody.AddForceAtPosition(Vector3.up * floatingPower * Mathf.Abs(diff), floaters[i].position, ForceMode.Force);
+                
+                float verticalVelocity = m_Rigidbody.GetPointVelocity(floaters[i].position).y;
+     
+                m_Rigidbody.AddForceAtPosition(Vector3.down * verticalVelocity * dampingPower, floaters[i].position, ForceMode.Force);
+                
+                floatersUnderWater++;
             }
-            if (underwater && floatersUnderWater == 0)
-            {
-                underwater = false;
-                SwitchState(false);
-            }   
         }
-    }
-
-    void SwitchState(bool isUnderwater)
-    {
-        if (isUnderwater)
-        {
-            m_Rigidbody.drag = underWaterDrag;
-            m_Rigidbody.angularDrag = underWaterAngularDrag;
-        }
-        else
-        {
-            m_Rigidbody.drag = airDrag;
-            m_Rigidbody.angularDrag = airAngularDrag;
-        }
-    }
-
-    private float getWaveHeightAtPoint(Vector3 position)
-    { 
-        LuxWaterUtils.GersterWavesDescription waveDescription = new LuxWaterUtils.GersterWavesDescription();
         
-        LuxWaterUtils.GetGersterWavesDescription(ref waveDescription, waterMaterial);
+        SwitchState();
+    }
 
-        Vector3 displacement = LuxWaterUtils.GetGestnerDisplacement(position, waveDescription, timeOffset);
-
-   
-        return displacement.y;
+    void SwitchState()
+    {
+        if (floaters.Length > 0)
+        {
+            float underwaterRatio = (float)floatersUnderWater / (float)floaters.Length;
+            
+            m_Rigidbody.drag = Mathf.Lerp(airDrag, underWaterDrag, underwaterRatio);
+            m_Rigidbody.angularDrag = Mathf.Lerp(airAngularDrag, underWaterAngularDrag, underwaterRatio);
+        }
     }
 }
