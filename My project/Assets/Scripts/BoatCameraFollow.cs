cfc5e7a5e27a4e0f13beaca9df9455b055a77634
Maciej <maciekjur123@gmail.com>
2025-06-30 14:52:45 +0200

    Fix camera and improve water trail effect

---
+++ My project/Assets/Scripts/BoatCameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BoatCameraFollow : MonoBehaviour
+{
+    public Transform boatTransform;
+    public Vector3 offset = new Vector3(-0.25f, 5.25f, -15f); 
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = boatTransform.position + boatTransform.rotation * offset;
+        
+        transform.position = targetPosition;
+
+        Quaternion boatYRotation = Quaternion.Euler(0f, boatTransform.eulerAngles.y, 0f);
+        transform.rotation = boatYRotation;
+    }
+}
